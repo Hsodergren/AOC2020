08e7f982c9adb558b6076a0d72b91e4f905692a4
henrik <henrik@localhost.localdomain>
2020-12-15 12:18:44 +0100

    Improved performace using Hashtbl instead of Map (day15)

--- lib/day15.ml
+++ lib/day15.ml
@@ -1,32 +1,19 @@
 open Core
 
-let next_num last_num map turn =
-  let next = match Int.Map.find map last_num with
-    | Some v -> turn-v
-    | None -> 0
-  in
-  next, Int.Map.set map ~key:last_num ~data:turn
-
 let play start num_turns =
-  let init_map, last_vs, turn = List.foldi start
-      ~f:(fun i (map,_,_) v ->
-          (Int.Map.set map ~key:v ~data:(i+1), v,i+1)
-        )
-      ~init:(Int.Map.empty,0, 1)
-  in
-  let rec aux map last_vs turn =
-    if turn = num_turns then  last_vs else
-    let next_vs = match Int.Map.find map last_vs with
-      | Some v -> (turn-v)
+  let map = Hashtbl.create (module Int) in
+  let last_vs, turn = ref 0, ref 0 in
+  List.iteri start ~f:(fun i v -> Hashtbl.set map ~key:v ~data:(i+1); last_vs := v; turn := i+1);
+  let rec aux prev_vs turn =
+    if turn = num_turns then prev_vs else
+    let next_vs = match Hashtbl.find map prev_vs with
+      | Some v -> turn-v
       | None -> 0
     in
-    aux
-      (Int.Map.set map ~key:last_vs ~data:turn)
-      next_vs
-      (turn+1)
+    Hashtbl.set map ~key:prev_vs ~data:turn;
+    aux next_vs (turn+1)
   in
-  aux init_map last_vs turn
-
+  aux !last_vs !turn
 
 let parse str =
   List.map ~f:int_of_string @@ String.split str ~on:','
